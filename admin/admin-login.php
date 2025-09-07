9700ea698577ade59f94aebbcc1a2705f46fb014
Hanzsx-Code <hanungsaw159@gmail.com>
2025-09-07 22:48:00 +0700

    Styling Admin Login

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FastBite [Admin Login]</title>
     <link rel="stylesheet" href="../assets/css/admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
@@ -12,8 +12,9 @@
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="wrapper">
         <div class="p-4 " style="max-width: 500px; width: 100%;">
-        <h2 class="mb-4 text-center">Admin Login</h2>
-        <div class="form-floating mb-3">
+        <h1 class="mb-4 text-center" style="font-weight: 700;">Admin Login</h1>
+        <form action="admin-login.php">
+            <div class="form-floating mb-3">
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Email address</label>
         </div>
@@ -21,6 +22,7 @@
             <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
         </div>
+        </form>
         <button class="btn btn-primary w-100">Login</button>
     </div>
     </div>
